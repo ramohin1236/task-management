36cbc20576092291e282d5c7859753540bedcd42
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2023-12-24 14:50:52 -0800

    firebase added

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,10 +1,29 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import { auth } from "../firebase/firebase.config";
 
+export const AuthContext = createContext(null)
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading]=useState(true)
+      
 
-const AuthProvider = () => {
+    const createUser =(email,password)=>{
+        setLoading(true)
+        return createBrowserRouter(auth,email,password)
+    }
+
+   const userInfo ={
+    loading,
+    user,
+    createUser
+   }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={userInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
